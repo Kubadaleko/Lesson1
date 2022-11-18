bf2fbe7ecfaaeb1e462ff44a549663aef9f73577
Kubadaleko <7757535@mail.ru>
2022-11-18 19:24:34 +0300

    Задача 7

---
+++ 04/Program.cs
@@ -0,0 +1,22 @@
+﻿// 7. Выяснить является ли число чётным.
+
+System.Console.Write("Введите любое целое число: ");
+int a = Convert.ToInt32(Console.Read());
+
+if (a % 2 == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
+
+// еще такой вариант
+
+System.Console.Write("Введите любое целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    System.Console.WriteLine($"{a} четное число");
+}
+else
+{
+    System.Console.WriteLine($"{a} нечетное число");
+}
+
+
