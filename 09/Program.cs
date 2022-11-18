49812d34a2b57708c03fe24a2de1a3d79b8287c2
Kubadaleko <7757535@mail.ru>
2022-11-18 19:27:53 +0300

    Задача 12

---
+++ 09/Program.cs
@@ -0,0 +1,30 @@
+﻿// 12. С клавиатуры вводится целое число из  диапазона  [10, 99]. 
+// Показать наибольшую цифру числа
+
+System.Console.Write("Введите число от 10 до 99 включительно: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int b;
+int c;
+if (a >= 10 && a <= 99)
+{
+    b = a / 10;
+    c = a % 10;
+
+    if (b > c)
+    {
+        System.Console.WriteLine($"{b} наибольшее");
+    }
+    else if (c > b)
+    {
+        System.Console.WriteLine($"{c} наибольшее");
+    }
+    else if (c == b)
+    {
+        System.Console.WriteLine("Числа равны");
+    }
+}
+else
+{
+    System.Console.WriteLine("Введенное число вне указанного диапазона");
+}
