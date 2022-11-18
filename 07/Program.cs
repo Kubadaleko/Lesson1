56a1d611b02a0994964a4703ee2e35c5b6ad1031
Kubadaleko <7757535@mail.ru>
2022-11-18 19:27:10 +0300

    Задача 15

---
+++ 07/Program.cs
@@ -0,0 +1,16 @@
+﻿// 15. С клавиатуры вводится целое число. Вывести третью цифру // числа или сообщить, что её нет (Вывести: NO).
+
+System.Console.WriteLine("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a <= 99)
+{
+    System.Console.WriteLine("NO");
+}
+else
+{
+    while (a > 1000)
+    {
+        a = a / 10;
+    }
+    System.Console.WriteLine(a % 10);
+}
